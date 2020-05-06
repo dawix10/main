cad9fd2cc3dd057143fed9a6b2e555555ab4da92
dawix10 <dawid.pustowka@gmail.com>
2020-05-06 10:59:47 +0200

    Update my_script.js

--- js/my_script.js
+++ js/my_script.js
@@ -3,16 +3,16 @@ var hot = false;
 
 
 if (true) {
-  console.log("STARTING...")
+  console.log("STARTING...");
 
-temp = prompt("Temperature readed from temperature sensor: ")
+temp = prompt("Temperature readed from temperature sensor: ");
 if (temp>30){
-  alert("Pajączki mają za ciepło!!! Wyłączamy grzanie!!!")
+  alert("Pajączki mają za ciepło!!! Wyłączamy grzanie!!!");
 else if(temp<22){
-  alert("Pajączki mają za zimno!!! Włączamy grzanie!!!")
+  alert("Pajączki mają za zimno!!! Włączamy grzanie!!!");
 }
 else {
-  alert("Pajączkom jest git!!! Nie rób nic!!!")
+  alert("Pajączkom jest git!!! Nie rób nic!!!");
 }
 }
 }
