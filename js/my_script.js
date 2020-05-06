4303a56c8ba00c502e951adf17a45c31ef24190c
dawix10 <dawid.pustowka@gmail.com>
2020-05-06 14:12:42 +0200

    Update my_script.js

--- js/my_script.js
+++ js/my_script.js
@@ -1,9 +1,8 @@
 var temp;
 var hot = false;
 
-
 if (true) {
   console.log("STARTING...");
 
-temp = console.prompt("Temperature readed from temperature sensor: ");
+temp = prompt("Temperature readed from temperature sensor: ");
 }
